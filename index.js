f46a418f437048f9401188e16b2f635a5b385032
fikar46 <zulfikar@doku.com>
2022-02-15 20:26:42 +0700

    add privatekey

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
-app.use(compression())
+app.use(compression())  
 // ----------------- MYSQL CONFIG -----------------------
 const conn = require('./database'); 
 app.get('/', (req,res) => {
