1d323e5b0dcd0d816f09b7c8f8f9243bfaeaec11
fikar46 <mzulfikarmey@gmail.com>
2022-03-27 13:43:50 +0700

    change password

--- routers/auth.js
+++ routers/auth.js
@@ -7,7 +7,7 @@ router.post('/register', authController.register);
 router.post('/signin',  authController.signin);
 router.post('/keeplogin', jwtMiddleware,authController.keeplogin);
 router.get('/getlistadminkunci',authController.getListUser);
-router.post('/changepassword',authController.changePassword);
+router.post('/changepassword',cacheFlushAll(),authController.changePassword);
 router.post('/changestatususer',cacheUniqueDelete('user_data'),authController.changeStatusUser);
 
 module.exports = router;
